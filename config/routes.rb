7c1f3dee0a937d006d5d71558934ba6627c6efa1
natsuod <odanatsumi@OdanoMac-mini.local>
2019-04-04 11:38:38 +0900

    Add-new-action-to-events-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'top#index'
   resources :news, only: [:index, :show, :new]
-  resources :events, only: [:index, :show]
+  resources :events, only: [:index, :show, :new]
 end
