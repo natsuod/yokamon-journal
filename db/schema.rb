fd298892bd72303f59fba5a44022bb90a791af13
natsuod <odanatsumi@OdanoMac-mini.local>
2019-04-12 14:39:43 +0900

    Add-nickname-to-user-db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190410053306) do
+ActiveRecord::Schema.define(version: 20190412023005) do
 
   create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "e_title", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20190410053306) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
