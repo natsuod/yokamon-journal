46dfcc02507ac67c70b9bd4e574914d645e1b42d
natsuod <odanatsumi@OdanoMac-mini.local>
2019-04-17 16:56:07 +0900

    Add-create-action-and-params-to-events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,6 +9,20 @@ class EventsController < ApplicationController
   end
 
   def new
-    @events = Event.new
+    @event = Event.new
   end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:e_title, :time, :date, :e_body, :e_image, :host, :place, :price)
+  end
+
 end
