fd21226da5160053eeffb2b34bb666e01e6183cc
natsuod <odanatsumi@OdanoMac-mini.local>
2019-04-17 16:55:41 +0900

    Add-create-action-to-events-of-routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :topics, only: [:index, :show, :new, :create]
-  resources :events, only: [:index, :show, :new]
+  resources :events, only: [:index, :show, :new, :create]
   resources :abouts, only: [:index]
   resources :contacts, only: [:index]
 end
