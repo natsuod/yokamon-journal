bb8ebd0d5e3c733d581c93bb20322220dcfd2d36
natsuod <odanatsumi@OdanoMac-mini.local>
2019-04-17 13:20:43 +0900

    Create-index.js-to-set-jquery

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.autoplay').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    pauseOnHover: false,
+    centerMode: true,
+  });
+});
