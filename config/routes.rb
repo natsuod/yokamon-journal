06814590329c80ecb72fd1498bbc887782767e05
natsuod <odanatsumi@OdanoMac-mini.local>
2019-04-24 16:48:44 +0900

    Add-update-action-to-event-routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :topics, only: [:index, :show, :new, :create, :destroy, :edit, :update]
-  resources :events, only: [:index, :show, :new, :create, :destroy, :edit]
+  resources :events, only: [:index, :show, :new, :create, :destroy, :edit, :update]
   resources :abouts, only: [:index]
   resources :contacts, only: [:index]
 end
