b7afe5b2c6a3e0ab6cebe2113dac04111d0e679f
natsuod <odanatsumi@OdanoMac-mini.local>
2019-04-24 15:44:52 +0900

    Add-destroy-action-to-topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
-  resources :topics, only: [:index, :show, :new, :create]
+  resources :topics, only: [:index, :show, :new, :create, :destroy]
   resources :events, only: [:index, :show, :new, :create]
   resources :abouts, only: [:index]
   resources :contacts, only: [:index]
